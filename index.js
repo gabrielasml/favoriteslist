15980335f779b3f085ea4d7d2e1927c57628c7b1
Apple <apple@Apples-MacBook-Pro-75.local>
2019-07-29 09:16:32 -0700

    firstupload

---
+++ index.js
@@ -0,0 +1,9 @@
+var button = $('.button');
+var input = $('input').val("");
+var input = $('bodyinput').val("");
+button.on('click', printlist);
+
+function printlist(){
+  var input = $('input').val("");
+  
+}
